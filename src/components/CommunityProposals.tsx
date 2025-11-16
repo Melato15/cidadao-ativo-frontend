3cbcda9cf58ffcd234d8f3b73acd1b54e52cf1f2
Guilherme Melato <gmelato28@gmail.com>
2025-11-16 15:14:58 -0300

    Add: proposal category

--- src/components/CommunityProposals.tsx
+++ src/components/CommunityProposals.tsx
@@ -110,7 +110,7 @@ const CommunityProposals: React.FC = () => {
         'Saúde': 'health',
         'Cultura': 'culture',
         'Transporte': 'transportation',
-        'Diversidades': 'diversity',
+        'Diversidade': 'diversity',
         'Outros': 'other'
       };
 
